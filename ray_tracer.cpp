2d182842cf585a52c175dc78ed817628326c864c
Eric Blancas <ericblancas23@gmail.com>
2019-02-11 12:16:57 -0800

    finished struct sphere

--- ray_tracer.cpp
+++ ray_tracer.cpp
@@ -16,23 +16,31 @@ struct Vec3 {
 
 inline double dot(const Vec3& a, const Vec3& b) {
     return (a.x*b.x + a.y*b.y + a.z*b.z);
-}
+};
 
 struct Ray {
     Vec3, o,d;
     Ray(const Vec3& o, const Vec3& d) : o(o), d(d) {}
-}
+};
 
-// struct Sphere {
-//     Vec3 c;
-//     double r;
-//     Sphere(const Vec3& c, double r) : c(c), r(r) {}
-//     Vec3 getNormal(const Vec3& pi) const {return (pi -c) / r;}
-//     bool intersect(const Ray& ray, double &t) {
-//         const Vec3 o = ray.o;
-//         const Vec3 d = ray.d;
-// 				const Vec3 oc  o - c;
-// 				const double b = 2 * dot(oc, d);
-//     }
-// }
+struct Sphere {
+    Vec3 c;
+    double r;
+    Sphere(const Vec3& c, double r) : c(c), r(r) {}
+    Vec3 getNormal(const Vec3& pi) const {return (pi -c) / r;}
+    bool intersect(const Ray& ray, double &t) {
+        const Vec3 o = ray.o;
+        const Vec3 d = ray.d;
+				const Vec3 oc  o - c;
+				const double b = 2 * dot(oc, d);
+				const double c = dot(oc, oc) - r*r;
+				double disc = b*b - 4 * c;
+				if (disc < ie-4) return false;
+				disc = sqrt(disc);
+				const double t0=-b - disc;
+				const double t1 = -b + disc;
+				t = (t0 < t1) ? t0 : t1;
+				return true;
+    }
+};
 
